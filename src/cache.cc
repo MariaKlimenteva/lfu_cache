11f1dde090d1612d4a5c17ca5e172356f84e1c29
Maria K <klimenteva.mr@phystech.edu>
2023-09-03 22:10:07 +0300

    changed the input and output format

--- src/cache.cc
+++ src/cache.cc
@@ -39,27 +39,25 @@ int main()
 {
     testing::InitGoogleTest();
 
-    // size_t cache_size;
-    // int number_of_elementes;
-    // int hits = 0;
+    size_t cache_size;
+    int number_of_elementes;
+    int hits = 0;
     
-    // std::cin >> cache_size >> number_of_elementes;
+    std::cin >> cache_size >> number_of_elementes;
 
-    // Cache<int> lfu{cache_size};
+    Cache<int> lfu{cache_size};
 
-    // std::cout << "Now start entering the elements: ";
+    for(int i = 0; i < number_of_elementes; i++)
+    {
+        int elem;
+        std::cin >> elem;
+        if(lfu.lookup_update(elem))
+        {
+            hits += 1;
+        };
+    }
 
-    // for(int i = 0; i < number_of_elementes; i++)
-    // {
-    //     int elem;
-    //     std::cin >> elem;
-    //     if(lfu.lookup_update(elem))
-    //     {
-    //         hits += 1;
-    //     };
-    // }
-
-    // std::cout << "Number of hits: " << hits << "\n";
+    std::cout << hits << "\n";
 
     return RUN_ALL_TESTS();
 }
