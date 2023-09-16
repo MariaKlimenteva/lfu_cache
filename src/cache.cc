96ee9f9ebc2e855da189781f515960293482d585
Maria K <klimenteva.mr@phystech.edu>
2023-09-16 23:34:15 +0300

    start to add e2e tests

--- src/cache.cc
+++ src/cache.cc
@@ -6,49 +6,83 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 //--------------------------------------------------------------------------
-TEST(CheckValues, Subtest_1) 
-{
-    std::ifstream tests_file;
-    tests_file.open("tests.txt");
+// TEST(CheckValues, Subtest_1) 
+// {
+//     std::ifstream tests_file;
+//     tests_file.open("tests.txt");
 
-    size_t cache_size;
-    int number_of_elementes;
-    int hits = 0;
+//     size_t cache_size;
+//     int number_of_elementes;
+//     int hits = 0;
 
-    Cache<int> lfu{cache_size};
+//     Cache<int> lfu{cache_size};
 
-    tests_file >> cache_size >> number_of_elementes;
+//     tests_file >> cache_size >> number_of_elementes;
 
-    for(int i = 0; i < number_of_elementes; i++)
-    {
-        int elem;
-        tests_file >> elem;
-        if(lfu.lookup_update(elem))
-        {
-            hits += 1;
-        };
-    }
+//     for(int i = 0; i < number_of_elementes; i++)
+//     {
+//         int elem;
+//         tests_file >> elem;
+//         if(lfu.lookup_update(elem))
+//         {
+//             hits += 1;
+//         };
+//     }
 
-    int answer;
-    tests_file >> answer;
-    EXPECT_EQ(hits, answer);
-    tests_file.close();
-}
+//     int answer;
+//     tests_file >> answer;
+//     EXPECT_EQ(hits, answer);
+//     tests_file.close();
+// }
 //--------------------------------------------------------------------------
-int main()
+int main(int argc, char** argv)
 {
-    #ifdef TEST_PARAM
-    testing::InitGoogleTest();
-    #endif
-    #ifndef TEST_PARAM
-
     size_t cache_size;
     int number_of_elements;
     int hits = 0;
     int perfect_hits = 0;
-    
-    std::cin >> cache_size >> number_of_elements;
+    //----------------------------------------------------------------------
+    #ifdef TEST_PARAM
+    std::ifstream tests;
+    tests.open(argv[1]);
 
+    while(!tests.eof())
+    {
+        if(tests)
+        {
+            tests >> cache_size >> number_of_elements;
+        }
+
+        Cache<int> lfu{cache_size};
+        Perfect_Cache<int> perfect{cache_size};
+
+        for(int i = 0; i < number_of_elements; i++)
+        {
+            int elem;
+            tests >> elem;
+            if(lfu.lookup_update(elem))
+            {
+                hits += 1;
+            }
+
+            perfect.make_list(elem);
+        }
+        
+        perfect.make_map();
+
+        for(auto elem : perfect.all_elements)
+        {
+            if(perfect.perfect_hit_counter(elem, number_of_elements))
+            {
+                perfect_hits += 1;
+            }
+        }
+    }
+    #endif
+    //----------------------------------------------------------------------
+    #ifndef TEST_PARAM
+    std::cin >> cache_size >> number_of_elements;
+    
     Cache<int> lfu{cache_size};
     Perfect_Cache<int> perfect{cache_size};
 
@@ -73,14 +107,9 @@ int main()
             perfect_hits += 1;
         }
     }
-
-    std::cout << "Hits of LFU cache: " << hits << "\n";
-    std::cout << "Hits of perfect cache: " << perfect_hits << "\n";
-
     #endif
-
-    #ifdef TEST_PARAM
-    return RUN_ALL_TESTS();
-    #endif
+    //----------------------------------------------------------------------
+    std::cout << hits << " ";
+    std::cout << perfect_hits << "\n";
 }
 //--------------------------------------------------------------------------
